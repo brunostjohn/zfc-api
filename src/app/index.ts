8b830f14b9290ce0b006443364389abb202ded3e
Bruno St. John <brunost.john@icloud.com>
2023-06-22 00:11:13 +0100

    feat: everything is in, just gotta cook

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+const db = new Sequelize(`mysql://root:${process.env.MYSQL_PWD}@db:3306/zfc`);
 
 const app = express();
 
